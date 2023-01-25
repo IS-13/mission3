f32b9775bf0a650cc2cadcd750eaf5514b310064
Blake Pereira <bp347@byu.edu>
2023-01-25 09:32:23 -0700

    final change

---
+++ Tic_Tac_toe/Tic_Tac_toe/Amy_and_Blake.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tic_Tac_toe
+{
+    public class Amy_and_Blake
+    {
+        public void printBoard(int[] board)
+        {
+            string[] arry = { "0", "1", "2", "3", "4", "5", "6", "7", "8"};
+
+            for(int i = 0; i < 9; i++)
+            {
+                if (board[i] == 1)
+                {
+                    arry[i] = "X";
+                }
+                if (board[i] == 2)
+                {
+                    arry[i] = "O";
+                }
+
+            }
+            Console.WriteLine(arry[0] + " | " + arry[1] + " | " + arry[2] + "\n" +
+                "---------" + "\n" +
+                arry[3] + " | " + arry[4] + " | " + arry[5] + "\n" +
+                "---------" + "\n" +
+                arry[6] + " | " + arry[7] + " | " + arry[8]);
+        }
+
+        public int checkWinner(int[] board)
+        {
+            int winnerNum = 0;
+            for (int i = 1; i < 3; i++)
+            {
+                //check columns
+                if ((board[0] == i) && (board[3] == i) && (board[6] == i))
+                {
+                    winnerNum = i;
+                }
+                if ((board[1] == i) && (board[4] == i) && (board[7] == i))
+                {
+                    winnerNum = i;
+                }
+                if ((board[2] == i) && (board[5] == i) && (board[8] == i))
+                {
+                    winnerNum = i;
+                }
+                //check in a row
+                if ((board[0] == i) && (board[1] == i) && (board[2] == i))
+                {
+                    winnerNum = i;
+                }
+                if ((board[3] == i) && (board[4] == i) && (board[5] == i))
+                {
+                    winnerNum = i;
+                }
+                if ((board[6] == i) && (board[7] == i) && (board[8] == i))
+                {
+                    winnerNum = i;
+                }
+                //check diagonals
+                if ((board[0] == i) && (board[4] == i) && (board[8] == i))
+                {
+                    winnerNum = i;
+                }
+                if ((board[2] == i) && (board[4] == i) && (board[6] == i))
+                {
+                    winnerNum = i;
+                }
+            }
+                
+            return winnerNum;
+        }
+    }
+}
--- Tic_Tac_toe/Tic_Tac_toe/Program.cs
+++ Tic_Tac_toe/Tic_Tac_toe/Program.cs
@@ -10,6 +10,55 @@ namespace Tic_Tac_toe
     {
         static void Main(string[] args)
         {
+            Amy_and_Blake ab = new Amy_and_Blake();
+
+            Console.WriteLine("Welcome to Tic-Tac-Toe!");
+            int[] board = new int[9];
+            int selection = 0;
+            int value = 0;
+            ab.printBoard(board);
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Player 1, please select a number:");
+                selection = int.Parse(Console.ReadLine());
+                board[selection] = 1;
+                ab.printBoard(board);
+                value = ab.checkWinner(board);
+                if (value == 1)
+                {
+                    Console.WriteLine("Player 1 is the winner! Congratulations!");
+                    break;
+                }
+                else if (value == 2)
+                {
+                    Console.WriteLine("Player 2 is the winner! Congratulations!");
+                    break;
+                }
+                if (i == 4)
+                {
+                    break;
+                }
+                Console.WriteLine("Player 2, please select a number:");
+                selection = int.Parse(Console.ReadLine());
+                board[selection] = 2;
+                ab.printBoard(board);
+                value = ab.checkWinner(board);
+                if (value == 1)
+                {
+                    Console.WriteLine("Player 1 is the winner! Congratulations!");
+                    break;
+                }
+                else if (value == 2)
+                {
+                    Console.WriteLine("Player 2 is the winner! Congratulations!");
+                    break;
+                }
+            }
+            value = ab.checkWinner(board);
+            if (value == 0)
+            {
+                Console.WriteLine("It's a tie game!");
+            }
         }
     }
 }
